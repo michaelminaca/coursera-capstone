8a1575ac57adeae6e6cf80a9d83c9dd9d0a09edd
Michael Minaca <michaelsminaca@gmail.com>
2023-03-22 22:24:53 +1100

    added nav bar scaffold and semantic html elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import logo from './assets/images/Logo.svg';
+import { ChakraProvider, HStack } from '@chakra-ui/react';
 
 function App() {
+  const navStyle = {
+    height: '70px',
+    backgroundColor: 'black',
+  };
+
   return (
-    <div className="App">
-      <p>React Working!</p>
-    </div>
+    <ChakraProvider>
+      <nav style={navStyle}>
+        <HStack
+          height="100%"
+          width="100%"
+          spacing="24px"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <img src={logo} alt="logo" />
+          <a href="./">HOME</a>
+          <a href="./">ABOUT</a>
+          <a href="./">MENU</a>
+          <a href="./">RESERVATIONS</a>
+          <a href="./">ORDER ONLINE</a>
+        </HStack>
+      </nav>
+      <main></main>
+      <footer></footer>
+    </ChakraProvider>
   );
 }
 
