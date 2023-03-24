dc67cde8841add78cbb938d538df6a665d2b84cc
Michael Minaca <michaelsminaca@gmail.com>
2023-03-24 12:55:04 +1100

    added form and validation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import logo from './assets/images/Logo.svg';
-import { ChakraProvider, HStack } from '@chakra-ui/react';
+import lemonDessert from './assets/images/lemon-dessert.jpg';
+import { ChakraProvider, HStack, VStack } from '@chakra-ui/react';
 
 function App() {
   const COLORS = {
@@ -11,6 +12,41 @@ function App() {
     height: '70px',
     backgroundColor: COLORS.primaryWhite,
     border: `1px solid black`,
+    fontFamily: 'Markazi Text, serif',
+    fontSize: '20px',
+  };
+
+  const leftContainerStyle = {
+    backgroundColor: COLORS.primaryGreen,
+    flex: 1,
+    height: '100%',
+  };
+
+  const reserveTableTextStyle = {
+    fontFamily: 'Markazi Text, serif',
+    color: COLORS.primaryWhite,
+    fontSize: '64px',
+    textAlign: 'center',
+    letterSpacing: '10px',
+    marginLeft: '20px',
+    marginRight: '20px',
+  };
+
+  const formTextStyle = {
+    fontFamily: 'Karla, sans-serif',
+    fontSize: '30px',
+  };
+
+  const submitButtonStyle = {
+    backgroundColor: COLORS.primaryGreen,
+    padding: '10px',
+    color: COLORS.primaryWhite,
+    borderRadius: '16px',
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(e);
   };
 
   return (
@@ -42,20 +78,48 @@ function App() {
             height: '100%',
           }}
         >
-          <div
-            style={{
-              backgroundColor: COLORS.primaryGreen,
-              flex: 1,
-              height: '100%',
-            }}
-          ></div>
+          <VStack
+            style={leftContainerStyle}
+            justifyContent="center"
+            alignItems="center"
+            spacing="20%"
+          >
+            <h1 style={reserveTableTextStyle}>Reserve a Table</h1>
+            <img
+              src={lemonDessert}
+              alt="lemon dessert"
+              style={{ width: '75%' }}
+            ></img>
+          </VStack>
           <div
             style={{
               backgroundColor: COLORS.primaryWhite,
               flex: 3,
               height: '100%',
             }}
-          ></div>
+          >
+            <form>
+              <VStack spacing="3%" justifyContent="center" alignItems="center">
+                <label style={{ ...formTextStyle, marginTop: '48px' }}>
+                  Name:
+                </label>
+                <input type="text" required></input>
+                <label style={formTextStyle}>Date: </label>
+                <input type="date" required></input>
+                <label style={formTextStyle}>Time: </label>
+                <input type="time" required></input>
+                <label style={formTextStyle}>Number of Diners: </label>
+                <input type="number" required min="1" max="32"></input>
+                <button
+                  type="submit"
+                  style={submitButtonStyle}
+                  onSubmit={(e) => submitHandler(e)}
+                >
+                  Submit
+                </button>
+              </VStack>
+            </form>
+          </div>
         </main>
         <footer></footer>
       </div>
